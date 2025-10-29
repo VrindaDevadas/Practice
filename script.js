84c70ec77a51285b788374a48de14cb6fa428375
VrindaDevadas <vrinda03devadas@gmail.com>
2025-10-29 15:08:49 +0530

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const student = {
+//     name: "Vrinda",
+//     age: 21,
+//     gender: "female"
+// };
+
+
+// student["age"] = student["age"] + 1;
+// student["name"] = "Sia"                       //assigning a new val
+
+// console.log(student.name);
+
+// console.log(student.age);
+
+
+// const product={
+//     name: "Parker Jotter Standard CT Ball pen",
+//     rating: 4,
+//     price: "₹270",
+//     offer: "5% off"
+
+// }
